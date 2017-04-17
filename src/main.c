71c84fda1b0b2e7c367fcf355b7095a065196820
Sanqui <gsanky@gmail.com>
2017-04-17 12:07:03 +0200

    Bootstrap source

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/pgmspace.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+
+#include <stdlib.h>
+
+int main() {
+    
+}
