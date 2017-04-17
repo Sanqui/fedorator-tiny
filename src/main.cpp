9e6bdaf20c524def180c496e6875e6f90488d82e
Sanqui <gsanky@gmail.com>
2017-04-17 16:59:43 +0200

    Use the esp8266-oled-ssd1306 library for OLED display
    Currently, a fork of the library is required for compatibility
    on my side.  This will be added to instructions later.

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,48 @@
 #define LED_BUILTIN 13
 #endif
 
+#include "SSD1306.h"
+
+#define ADDRESS 0x3C
+
+SSD1306  display(ADDRESS, -1, -1);
+
 void setup()
 {
+    Serial.begin(9600);
+    Serial.println();
+    Serial.println();
+    Serial.println("begin");
+    delay(200);
     pinMode(LED_BUILTIN, OUTPUT);
+    
+    
+    display.init();
+    display.flipScreenVertically();
+    
+    display.clear();
+
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 0, "Fedorator");
+
+    display.display();
+    
+    
 }
 
 void loop() {
+    Serial.println("loop");
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(300);
+    delay(200);
 
     digitalWrite(LED_BUILTIN, LOW);
     delay(200);
+    
+    //display.clear();
+    display.setColor(BLACK);
+    display.fillRect(0, 32, 32, 64);
+    display.setColor(WHITE);
+    display.drawString(0, 32, String(millis()));
+    display.display();
 }
